cee8efff81d35bd36cf7c08d49777d250551f8f0
kokoax <yomo4808@gmail.com>
2018-05-04 22:42:06 +0900

    Fix handler to controller in code/Fix controller from some refactoring

--- adapters/controllers/album_contoller.go
+++ adapters/controllers/album_contoller.go
@@ -1,38 +1,58 @@
 package controllers
 
 import (
+	"errors"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 
+	"github.com/Basabi-lab/lms/adapters/dbs"
 	"github.com/Basabi-lab/lms/adapters/presenters"
 	"github.com/Basabi-lab/lms/usecases"
 )
 
-type AlbumHandler struct {
-	usecases.AllAlbumUsecase
-	presenters.AllAlbumPresenter
+type AlbumController struct {
+	aau usecases.AllAlbumUsecaseExt
+	aap presenters.AllAlbumPresenterExt
 }
 
-func (h *AlbumHandler) getAllAlbum(c *gin.Context) {
-	albums, _ := h.AllAlbum(c)
-	json, _ := h.ToJson(*albums)
+func ResponseError(c *gin.Context, err error) {
+	c.Error(err)
+	c.JSON(http.StatusInternalServerError, gin.H{"Error": err})
+}
+
+func (h *AlbumController) getAllAlbum(c *gin.Context) {
+	albums, err := h.aau.AllAlbum(c)
+	if err != nil {
+		ResponseError(c, err)
+	}
+
+	json, err := h.aap.ToByteList(*albums)
+	if err != nil {
+		ResponseError(c, err)
+	}
 
 	c.Writer.Header().Set("Content-Type", "application/json")
 	c.Writer.WriteHeader(200)
-	c.Writer.Write(json.Json)
+	_, err = c.Writer.Write(json.JsonByteList)
+	if err != nil {
+		ResponseError(c, err)
+	}
 }
 
-func (h *AlbumHandler) getWithId(c *gin.Context) {
+func (h *AlbumController) getWithId(c *gin.Context) {
 	// TODO: contextからIDを取り出してGetByIDに渡す
 	// info, _ := h.GetById()
 	// h.GetWithID(info)
+	ResponseError(c, errors.New("not implement"))
 }
 
-func NewAlbumHandler(r *gin.Engine, db *gorm.DB) {
-	handler := AlbumHandler{
-		usecases.NewAllAlbumUsecase(db),
-		presenters.NewAllAlbumPresenter(db),
+func NewAlbumController(r *gin.Engine, db *gorm.DB) {
+	controller := AlbumController{
+		usecases.NewAllAlbumUsecase(dbs.NewAlbumMysql(db)),
+		presenters.NewAllAlbumPresenter(),
 	}
-	r.GET("/album", handler.getAllAlbum)
-	r.GET("/album/:id", handler.getWithId)
+	r.GET("/album", controller.getAllAlbum)
+	r.GET("/album/:id", controller.getWithId)
 }
