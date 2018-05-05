21d39c8bed7504128f1163b48b186d07c0494acf
kokoax <yomo4808@gmail.com>
2018-05-05 23:37:16 +0900

    Add album get by id presenter

---
+++ adapters/presenters/album_get_by_id_presenter.go
@@ -0,0 +1,32 @@
+package presenters
+
+import (
+	"encoding/json"
+
+	"github.com/Basabi-lab/lms/usecases"
+)
+
+type AlbumGetByIDPresenterExt interface {
+	ToByteList(acur usecases.AlbumGetByIDUsecaseResult) (*AlbumGetByIDPresenterResult, error)
+}
+
+type AlbumGetByIDPresenterResult struct {
+	JsonByteList []byte
+}
+
+type albumGetByIDPresenter struct{}
+
+func NewAlbumGetByIDPresenter() AlbumGetByIDPresenterExt {
+	return &albumGetByIDPresenter{}
+}
+
+func NewAlbumGetByIDPresenterResult(json []byte) *AlbumGetByIDPresenterResult {
+	return &AlbumGetByIDPresenterResult{
+		JsonByteList: json,
+	}
+}
+
+func (albumu *albumGetByIDPresenter) ToByteList(agbiu usecases.AlbumGetByIDUsecaseResult) (*AlbumGetByIDPresenterResult, error) {
+	json, _ := json.Marshal(agbiu.Album)
+	return NewAlbumGetByIDPresenterResult(json), nil
+}
---
+++ adapters/presenters/album_get_by_id_presenter_test.go
@@ -0,0 +1,25 @@
+package presenters
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/Basabi-lab/lms/domains/models"
+	"github.com/Basabi-lab/lms/usecases"
+)
+
+func TestAlbumGetByIDToJson(t *testing.T) {
+	agbip := NewAlbumGetByIDPresenter()
+	album := &models.Album{
+		Title: "Album title",
+		Genre: "Album Genre",
+		Year:  2000,
+	}
+	usecaseResult := usecases.AlbumGetByIDUsecaseResult{
+		Album: album,
+	}
+
+	_, err := agbip.ToByteList(usecaseResult)
+	assert.NoError(t, err)
+}
