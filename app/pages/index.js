c9f1379f6132a721adc8f10a9a9e6c8e1bc2d8eb
Eddie <tamamu.1r1s@gmail.com>
2018-05-07 16:00:52 +0900

    Add Sortable Pane

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,1 +1,31 @@
-export default () => <div>Welcome to Labo Music Server!</div>
+
+import React from 'react';
+import { SortablePane, Pane } from 'react-sortable-pane';
+
+
+export default () => {
+  const paneWidth = 240;
+  const paneStyle = {
+    color: "white",
+    backgroundColor: "black",
+    width: paneWidth,
+    height: "100%",
+    margin: 0,
+    padding: 0
+  };
+  return (
+    <div style={{width:"100vw",height:"100vh"}}>
+      <SortablePane
+        direction="horizontal"
+        margin={20}
+      >
+        <Pane id={0} key={0} style={paneStyle}>
+          <p>0</p>
+        </Pane>
+        <Pane id={1} key={1} style={paneStyle}>
+          <p>1</p>
+        </Pane>
+      </SortablePane>
+    </div>
+  );
+};
