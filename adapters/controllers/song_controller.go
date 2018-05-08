efdbddf4c68dbcba1de9e9769f7de218e33637b6
kokoax <yomo4808@gmail.com>
2018-05-08 18:00:22 +0900

    Fix some

--- adapters/controllers/song_controller.go
+++ adapters/controllers/song_controller.go
@@ -1,1 +1,102 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/Basabi-lab/lms/adapters/presenters"
+	"github.com/Basabi-lab/lms/usecases"
+)
+
+type SongControllerExt interface {
+	GetAll(c *gin.Context)
+	GetById(c *gin.Context)
+	Post(c *gin.Context)
+}
+
+type songController struct {
+	sau   usecases.AllSongUsecaseExt
+	sap   presenters.AllSongPresenterExt
+	sgbiu usecases.SongGetByIDUsecaseExt
+	sgbip presenters.SongGetByIDPresenterExt
+	scu   usecases.SongCreateUsecaseExt
+	scp   presenters.SongCreatePresenterExt
+}
+
+func NewSongController(
+	sau usecases.AllSongUsecaseExt,
+	sap presenters.AllSongPresenterExt,
+	sgbiu usecases.SongGetByIDUsecaseExt,
+	sgbip presenters.SongGetByIDPresenterExt,
+	scu usecases.SongCreateUsecaseExt,
+	scp presenters.SongCreatePresenterExt,
+) SongControllerExt {
+
+	return &songController{
+		sau:   sau,
+		sap:   sap,
+		sgbiu: sgbiu,
+		sgbip: sgbip,
+		scu:   scu,
+		scp:   scp,
+	}
+}
+
+func (h *songController) GetAll(c *gin.Context) {
+	songs, err := h.sau.All(c)
+	if err != nil {
+		ResponseError(c, err)
+		return
+	}
+
+	json, err := h.sap.ToByteList(songs)
+	if err != nil {
+		ResponseError(c, err)
+		return
+	}
+
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.WriteHeader(200)
+	_, err = c.Writer.Write(json.JsonByteList)
+	if err != nil {
+		ResponseError(c, err)
+		return
+	}
+}
+
+func (h *songController) GetById(c *gin.Context) {
+	song, err := h.sgbiu.GetByID(c)
+	if err != nil {
+		ResponseError(c, err)
+		return
+	}
+
+	json, err := h.sgbip.ToByteList(song)
+	if err != nil {
+		ResponseError(c, err)
+		return
+	}
+
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.WriteHeader(200)
+	_, err = c.Writer.Write(json.JsonByteList)
+	if err != nil {
+		ResponseError(c, err)
+		return
+	}
+}
+
+func (h *songController) Post(c *gin.Context) {
+	cRes, err := h.scu.Create(c)
+	if err != nil {
+		ResponseError(c, err)
+		return
+	}
+
+	pRes, err := h.scp.Response(cRes)
+	if err != nil {
+		ResponseError(c, err)
+		return
+	}
+
+	c.JSON(200, pRes.Res)
+}
