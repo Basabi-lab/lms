cf0d9bac3ac7f10a94c2b261b99b02e0a90a5222
Eddie <tamamu.1r1s@gmail.com>
2018-05-08 17:52:04 +0900

    Modify to use state

--- app/pages/album-card-list.js
+++ app/pages/album-card-list.js
@@ -3,8 +3,17 @@ import React from 'react'
 import AlbumCard from './album-card'
 
 export default class AlbumCardList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { cardData: props.cardData };
+  }
+  add(album) {
+    let data = this.state.cardData;
+    data.push(album);
+    this.setState({cardData: data})
+  }
   render() {
-    var CardNodes = this.props.cardData.map((card) => {
+    let CardNodes = this.state.cardData.map((card) => {
       return (
         <AlbumCard albumId={card.id} artistId={card.artist_id} title={card.title} genre={card.genre} year={card.year} />
       );
