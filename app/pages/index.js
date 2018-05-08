e90de5a96c3babbaa62e0df79a094b5d004a5c44
Eddie <tamamu.1r1s@gmail.com>
2018-05-08 17:40:50 +0900

    Append AlbumCardList into the pane

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,29 +1,44 @@
 
 import React from 'react';
 import { SortablePane, Pane } from 'react-sortable-pane';
+import AlbumCardList from './album-card-list'
+
+const paneWidth = 240;
+const paneStyle = {
+  color: "white",
+  backgroundColor: "black",
+  width: paneWidth,
+  height: "100%"
+};
+
+const data = [
+  {
+    id: 0,
+    title: "Hello world",
+    artist_id: 0,
+    genre: "Rock",
+    year: 2014
+  },
+  {
+    id: 1,
+    title: "Overflow",
+    artist_id: 1,
+    genre: "Rock",
+    year: 2013
+  }
+];
 
 
 export default () => {
-  const paneWidth = 240;
-  const paneStyle = {
-    color: "white",
-    backgroundColor: "black",
-    width: paneWidth,
-    height: "100%",
-    margin: 0,
-    padding: 0
-  };
   return (
     <div style={{width:"100vw",height:"100vh"}}>
-      <SortablePane
-        direction="horizontal"
-        margin={20}
-      >
-        <Pane id={0} key={0} style={paneStyle}>
-          <p>0</p>
+      <SortablePane direction="horizontal" margin={20}>
+        <Pane id={0} key={0} width={240} height="100%" style={paneStyle}>
+          <p>Albums</p>
+          <AlbumCardList cardData={data} />
         </Pane>
-        <Pane id={1} key={1} style={paneStyle}>
-          <p>1</p>
+        <Pane id={1} key={1} width={240} height="100%" style={paneStyle}>
+          <p>Songs</p>
         </Pane>
       </SortablePane>
     </div>
