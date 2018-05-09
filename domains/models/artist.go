61a23b82e8fbb802461023c7724defab3b9b2364
kokoax <yomo4808@gmail.com>
2018-05-10 00:31:23 +0900

    Fix ArtistはAlbumのIDを持たないので削除

--- domains/models/artist.go
+++ domains/models/artist.go
@@ -5,7 +5,6 @@ import "github.com/jinzhu/gorm"
 type Artist struct {
 	gorm.Model
 	Album     *Album `json:album`
-	AlbumID   uint   `json:album_id`
 	Name      string `json:title`
 	Biography string `json:biography`
 }
