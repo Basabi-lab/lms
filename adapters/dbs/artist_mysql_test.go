083ec95503b671f7273a792d54c596b80780b220
kokoax <yomo4808@gmail.com>
2018-05-09 17:57:35 +0900

    Add artist db accessor

---
+++ adapters/dbs/artist_mysql_test.go
@@ -0,0 +1,53 @@
+package dbs
+
+import (
+	"testing"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/stretchr/testify/assert"
+	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
+
+	"github.com/Basabi-lab/lms/domains/models"
+	"github.com/Basabi-lab/lms/test"
+)
+
+func TestArtistGetAll(t *testing.T) {
+	db, mock := test.ConnectMockDB("test_artist_get_all")
+	defer db.Close()
+
+	ar := NewArtistMysql(db)
+
+	artist := test.TestArtistData()
+
+	var artistCols []string = []string{"id", "created_at", "updated_at", "deleted_at", "name", "biography"}
+	mock.ExpectQuery("SELECT").WillReturnRows(sqlmock.NewRows(artistCols).
+		AddRow(artist.ID, artist.CreatedAt, artist.UpdatedAt, artist.DeletedAt, artist.Name, artist.Biography))
+
+	artists, err := ar.GetAll()
+	assert.NoError(t, err)
+
+	expect := []*models.Artist{}
+	expect = append(expect, artist)
+	assert.Equal(t, expect, artists)
+}
+
+func TestArtistGetByID(t *testing.T) {
+	db, mock := test.ConnectMockDB("test_artist_get_by_id")
+	defer db.Close()
+
+	ar := NewArtistMysql(db)
+
+	expect := test.TestArtistData()
+
+	var artistCols []string = []string{"id", "created_at", "updated_at", "deleted_at", "name", "biography"}
+	mock.ExpectQuery("SELECT").WillReturnRows(sqlmock.NewRows(artistCols).
+		AddRow(expect.ID, expect.CreatedAt, expect.UpdatedAt, expect.DeletedAt, expect.Name, expect.Biography))
+
+	artist, err := ar.GetByID(0)
+	assert.NoError(t, err)
+
+	assert.Equal(t, expect, artist)
+}
+
+func TestArtistCreate(t *testing.T) {
+}
