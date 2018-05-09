70d340d7c3100e78076a3f173b82f127410c65b0
kokoax <yomo4808@gmail.com>
2018-05-09 17:57:50 +0900

    Add artist repository interface

---
+++ domains/repositories/artist_repository.go
@@ -0,0 +1,9 @@
+package repositories
+
+import "github.com/Basabi-lab/lms/domains/models"
+
+type ArtistRepository interface {
+	GetByID(id uint) (*models.Artist, error)
+	GetAll() ([]*models.Artist, error)
+	Create(cd *models.Artist) (uint, error)
+}
