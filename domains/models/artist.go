c0d969b2ce94c91aa831ae398465061569cb196c
kokoax <yomo4808@gmail.com>
2018-05-11 16:50:16 +0900

    Fix json化する際にApperCaseにってたので修正

--- domains/models/artist.go
+++ domains/models/artist.go
@@ -1,10 +1,8 @@
 package models
 
-import "github.com/jinzhu/gorm"
-
 type Artist struct {
-	gorm.Model
-	Album     *Album `json:album`
-	Name      string `json:title`
-	Biography string `json:biography`
+	Model
+	Album     []*Album `json:"album"`
+	Name      string   `json:"title"`
+	Biography string   `json:"biography"`
 }
--- domains/models/song.go
+++ domains/models/song.go
@@ -1,17 +1,15 @@
 package models
 
-import "github.com/jinzhu/gorm"
-
 type Song struct {
-	gorm.Model
-	Album    *Album  `gorm:"foreignkey:AlbumID";json:album`
-	AlbumID  uint    `json:album_id`
-	Artist   *Artist `gorm:"foreignkey:ArtistID";json:artist`
-	ArtistID uint    `json:artist_id`
-	Title    string  `json:title`
-	Genre    string  `json:genre`
-	Year     int     `json:year`
-	Track    int     `json:track`
-	AlbumNum int     `json:album_num`
-	Dir      string  `json:dir`
+	Model
+	Album    *Album  `gorm:"foreignkey:AlbumID" json:"album"`
+	AlbumID  uint    `json:"album_id"`
+	Artist   *Artist `gorm:"foreignkey:ArtistID" json:"artist"`
+	ArtistID uint    `json:"artist_id"`
+	Title    string  `json:"title"`
+	Genre    string  `json:"genre"`
+	Year     int     `json:"year"`
+	Track    int     `json:"track"`
+	Disc     int     `json:"disc"`
+	Path     string  `json:"path"`
 }
