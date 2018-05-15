80da196d3585d56f57ac7b2065e2a2801eed0486
kokoax <yomo4808@gmail.com>
2018-05-15 16:35:05 +0900

    Fix もうちょっとスマートに修正

--- tests/set_timestamp_song.sh
+++ tests/set_timestamp_song.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-find . | xargs touch --date="2018/5/12 00:00"
-touch --date="2018/5/12 18:00" ./song/not_updated1.mp3
-touch --date="2018/5/12 18:00" ./song/not_updated2.mp3
+find ../tests/song | xargs touch --date="2018/5/12 00:00"
+find ../tests/song | grep not_updated | xargs touch --date="2018/5/12 12:00"
 
